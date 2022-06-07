c645ef4e596390eec4a6993b74c83d2d82a02733
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-06-07 17:23:13 +0530

    minor changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,16 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-	const accounts = await hre.ethers.getSigners();
+	const accounts = await hre.ethers.getSigners()
 
 	for (const account of accounts) {
-		console.log(account.address);
+		console.log(account.address)
 	}
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -17,22 +19,21 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-// const rinkebyRpcUrl = process.env.RINKEBY_RPC_URL;
-// const privateKey = process.env.PRIVATE_KEY;
+const rinkebyRpcUrl = process.env.RINKEBY_RPC_URL
+const privateKey = process.env.PRIVATE_KEY
+const ganachUrl = process.env.GANACH_URL
 
 module.exports = {
 	defaultNetwork: "hardhat",
 	networks: {
 		rinkeby: {
-			url: "https://eth-rinkeby.alchemyapi.io/v2/_aHNaePZ0dri-_Shk1DmdNbpxwLWpe00",
-			accounts: [
-				"b24bbbc35e8aea1aeada7d8d5268456ab9e725071a584e5a7b84e231a2f62628",
-			],
+			url: rinkebyRpcUrl,
+			accounts: [privateKey],
 			chainId: 4,
 		},
 		ganache: {
-			url: "HTTP://127.0.0.1:7545",
+			url: ganachUrl,
 		},
 	},
 	solidity: "0.8.4",
-};
+}
